dcbccb4db83887dd4b5e73f47df4189170a09be1
dlee97 <59745305+dlee97@users.noreply.github.com>
2020-04-20 14:59:34 -0400

    Remade main code
    Created new object for code. Added function for changing sorting methods. Updated csv exporting method with new guide. Created progress updating print statements for code. Added sleep to limit speed and help prevent crashing when pulling large numbers of posts.

--- Scraper.py
+++ Scraper.py
@@ -1,33 +1,72 @@
+from os.path import isfile
 import praw
 import pandas as pd
+from time import sleep
 import datetime as dt
 
 reddit = praw.Reddit("credentials")
 
-subreddit = reddit.subreddit('Pics')
-
-top_subreddit = subreddit.top()
-
-topics_dict = { "title":[], "score":[], "id":[], "url":[], "comms_num":[], "created": [], "body":[]}
-
-for submission in top_subreddit:
-    topics_dict["title"].append(submission.title)
-    topics_dict["score"].append(submission.score)
-    topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
+class SubredditScraper:
     
-    topics_dict["created"].append(submission.created)
-    topics_dict["body"].append(submission.selftext)
+    def __init__(self, sub, sort='new', lim=100, mode='w'):
+        self.sub = sub
+        self.sort = sort
+        self.lim = lim
+        self.mode = mode
 
-topics_data = pd.DataFrame(topics_dict)
-topics_data
+        print(f'SubredditScraper instance created with values sub = {sub}, sort = {sort}, lim = {lim}, mode= {mode}')
+    
+    def set_sort(self):
+        if self.sort == 'new':
+            return self.sort, reddit.subreddit(self.sub).new(limit=self.lim)
+        elif self.sort == 'top':
+            return self.sort, reddit.subreddit(self.sub).top(limit=self.lim)
+        elif self.sort == 'top':
+            return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
+        else:
+            self.sort = 'hot'
+            print('Sort method was not recognized, defaulting to hot.')
+            return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
 
-def get_date(created):
-    return dt.datetime.fromtimestamp(created)
+    def get_posts(self):
 
-_timestamp = topics_data["created"].apply(get_date)
+        sub_dict = {'selftext': [], 'title': [], 'id': [], 'sorted_by': [], 'num_comments': [], 'score': [], 'created':[]}
+        csv = f'{self.sub}_posts.csv'
+        sort, subreddit = self.set_sort()
+        df, csv_loaded = (pd.read_csv(csv), 1) if isfile(csv) else ('',0)
 
-topics_data = topics_data.assign(timestamp = _timestamp)
+        print(f'csv = {csv}')
+        print(f'After set_sort(), sort = {sort} and sub = {self.sub}')
+        print(f'csv_loaded = {csv_loaded}')
 
-topics_data.to_csv('RedditScraped.csv', index=False)
+        print(f'Collecting information from r/{self.sub}.')
+
+        for post in subreddit:
+            unique_id = post.id not in tuple(df.id) if csv_loaded else True
+
+            if unique_id:
+                sub_dict['selftext'].append(post.selftext)
+                sub_dict['title'].append(post.title)
+                sub_dict['id'].append(post.id)
+                sub_dict['sorted_by'].append(sort)
+                sub_dict['num_comments'].append(post.num_comments)
+                sub_dict['score'].append(post.score)
+                sub_dict['created'].append(dt.datetime.fromtimestamp(post.created))
+            sleep(0.1)
+        
+        
+
+        new_df = pd.DataFrame(sub_dict)
+
+        if 'Dataframe' in str(type(df)) and self.mode == 'w':
+            pd.concat([df, new_df], axis=0, sort=0).tto_csv(csv, index=False)
+            print(
+                f'{len(new_df)} new posts were collected and saved to {csv}')
+        elif self.mode == 'w':
+            new_df.to_csv(csv, index=False)
+            print(f'{len(new_df)} posts collected and saved to {csv}')
+        else:
+            print(f'{len(new_df)} posts were collected but they were not added to {csv} because mode was set to "{self.mode}')
+
+if __name__ == '__main__':
+    SubredditScraper('pics',lim=100,mode='w',sort='top').get_posts()
