5a98238dd53d7386f504515700e03a91cacd3941
dlee97 <59745305+dlee97@users.noreply.github.com>
2020-04-21 12:16:00 -0400

    Added Commenting
    Added brief comments to explain the parts of the code.

--- Scraper.py
+++ Scraper.py
@@ -8,8 +8,13 @@ import datetime as dt
 import urllib.request
 from tkinter import *
 
+#Importing credentials to use Reddit's API
 reddit = praw.Reddit("credentials")
 
+"""
+This is the Reddit Scraper that scrapes submission text and images
+
+"""
 class SubredditScraper:
     
     def __init__(self, sub, sort, lim, mode):
@@ -20,6 +25,7 @@ class SubredditScraper:
 
         print(f'SubredditScraper instance created with values sub = {sub}, sort = {sort}, lim = {lim}, mode= {mode}')
     
+    #This is the method for changing the scraper based on the defined sort method
     def set_sort(self):
         if self.sort == 'new':
             return self.sort, reddit.subreddit(self.sub).new(limit=self.lim)
@@ -32,10 +38,12 @@ class SubredditScraper:
             ui.updates('Sort method was not recognized, defaulting to hot.')
             return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
     
+    #This is the image downloader
     def get_image(self,link):
         image_dir = 'reddit_images/'
         cwd = os.getcwd()
         directory = cwd + "/" + image_dir
+        #Checking for existing directory and created if non existant
         if os.path.isdir(directory):
             pass
         else:
@@ -45,6 +53,7 @@ class SubredditScraper:
             except OSError as error:
                 print("Directory '%s' can not be created")
         name = os.path.basename(link)
+        #This renames the file and downloads if able
         if name == "":
             pass
         elif name.isalnum() == False:
@@ -56,9 +65,10 @@ class SubredditScraper:
             if not os.path.isfile(filename):
                 urllib.request.urlretrieve(link, filename)
 
-        
+    #This is the actual scraper
     def get_posts(self):
 
+        #Setting dictonary for what information we want to scrape and checking csv
         sub_dict = {'selftext': [], 'title': [], 'id': [], 'sorted_by': [], 'num_comments': [], 'score': [], 'created':[], 'url':[]}
         csv = f'{self.sub}_posts.csv'
         sort, subreddit = self.set_sort()
@@ -70,6 +80,7 @@ class SubredditScraper:
 
         ui.updates(f'Collecting information from r/{self.sub}.')
 
+        #Scraping posts and calling image scraper if the post has not already been scraped
         for post in subreddit:
             unique_id = post.id not in tuple(df.id) if csv_loaded else True
 
@@ -85,8 +96,8 @@ class SubredditScraper:
                 self.get_image(post.url)
             sleep(0.1)
         
+        #Putting into pandas to export post data to csv
         new_df = pd.DataFrame(sub_dict)
-
         if 'Dataframe' in str(type(df)) and self.mode == 'w+':
             pd.concat([df, new_df], axis=0, sort=0).tto_csv(csv, index=False)
             ui.updates(f'{len(new_df)} new posts were collected and saved to {csv}')
@@ -96,17 +107,24 @@ class SubredditScraper:
         else:
             print(f'{len(new_df)} posts were collected but they were not added to {csv} because mode was set to "{self.mode}')
 
+"""
+This is the GUI for the Scraper
+
+"""
 class UserInterface(Frame):
 
     def __init__(self, master):
+        #loading everything
         Frame.__init__(self,master)
         self.master = master
         master.title('Reddit Scraper')
         self.inputs()
 
+        #Scraper button
         self.scrape_button = Button(self, text='Initiate Scrape', command=self.scrape)
         self.scrape_button.grid(row=3, column=1, sticky='S')
 
+        #setting up grid
         self.grid()
         col_count, row_count = self.grid_size()
         for col in range(col_count):
@@ -114,12 +132,15 @@ class UserInterface(Frame):
         for row in range(row_count):
             self.grid_rowconfigure(row, minsize=30)
 
+    #This sets up all the widgets for user input
     def inputs(self):
+        #Subreddit entry
         self.sub_entry = Entry(self, width=12, font=('Arial',10))
         self.sub_entry.grid(row=0, column=1)
         self.sub_lbl = Label(self, text='Enter the desired Subreddit:')
         self.sub_lbl.grid(row=0)
 
+        #Sort method
         sort_dict = {'top', 'new', 'hot'}
         self.tkvar = StringVar(root)
         self.tkvar.set('top')
@@ -128,11 +149,13 @@ class UserInterface(Frame):
         self.sort_lbl = Label(self, text='Select the sorting method: ')
         self.sort_lbl.grid(row=1)
 
+        #Post number limit
         self.lim_entry = Entry(self, width=8, font=('Arial',10))
         self.lim_entry.grid(row=2, column=1)
         self.lim_lbl = Label(self, text='Enter the amount of posts to download (Max 1000): ')
         self.lim_lbl.grid(row=2)
 
+    #This calls the scraper and checks for existing subreddit and that entries are filled out
     def scrape(self):
         sub_in = self.sub_entry.get()
         sort_in = self.tkvar.get()
@@ -147,17 +170,20 @@ class UserInterface(Frame):
         else:
             if limit_in <= 1000:
                 try:
+                    #Scraper is called
                     SubredditScraper(sub=sub_in,lim=limit_in,mode='w+',sort=sort_in).get_posts()
                 except prawcore.exceptions.Redirect:
                     self.updates(f'Please check that {sub_in} is an existing subreddit.')
             else:
                 self.updates('Error post download limit is 1000')
 
+    #This sets up the progress updates and error messaging in the GUI
     def updates(self,message):
         self.update_lbl = Label(self, text=message, foreground='red')
         self.update_lbl.grid(row=3, column=0, sticky='S')
         self.update()
-        
+
+#Calling up the UI if this file is being called directly
 if __name__ == '__main__':
     root = Tk()
     root.geometry()
